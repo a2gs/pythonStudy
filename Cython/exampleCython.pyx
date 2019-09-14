d321614c2d17355ebca1248e1544ee5195ab270b
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-09-14 17:10:09 -0300

    Cython sample

---
+++ Cython/exampleCython.pyx
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+cpdef int test(int x):
+	cdef int y = 0
+	cdef int i = 0
+
+	for i in range(x):
+		y += i
+
+	return y
---
+++ Cython/runTest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import exampleCython
+
+print(exampleCython.test(50))
