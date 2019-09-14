1f5b2a6fccee1718624a8c50751f6e6c573131fa
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-09-13 23:41:09 -0300

    Sample of Python native log system

---
+++ loggingModule.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+
+def func():
+	logging.debug('This is a debug message')
+	logging.info('This is an info message')
+	logging.warning('This is a warning message')
+	logging.error('This is an error message')
+	logging.critical('This is a critical message')
+
+	try:
+		c = 1 / 0
+	except Exception as e:
+		logging.exception("Exception occurred", exc_info=True) #exc_info to on/off this log
+
+logging.basicConfig(filename='log.text', filemode='a', level=logging.DEBUG, format='%(asctime)s %(msecs)d - %(process)d - %(levelname)s - %(filename)s:%(funcName)s:%(lineno)d - %(message)s', datefmt='%Y%m%d %H%M%S')
+func()
+logging.shutdown()
